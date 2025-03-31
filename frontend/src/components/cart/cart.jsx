f0fee9494be4da1a72eb4f9fcc05e80f288e4585
Bojan Kolano <bojan@forwardslashny.com>
2025-03-31 09:58:47 +0200

    Added shipping and price calculation

--- frontend/src/components/cart/cart.jsx
+++ frontend/src/components/cart/cart.jsx
@@ -6,7 +6,6 @@ import './cart.scss';
 export default function Cart() {
 
     const {cart, addToCart, removeFromCart} = useCart();
-
     const cartItems = cart.filter(item => item.quantity > 0);
 
     const handleIncrement = (product) => addToCart(product, product.quantity + 1);
@@ -32,33 +31,33 @@ export default function Cart() {
                     <p>Your cart is empty! 
                         <Link to='/shop'>Continue Shopping</Link>
                     </p>) : (
-                        <ul className="cart__items">
-                            {cartItems.map(item => (
-                                <li className="cart__item" key={item.id}>
-                                    <Link className="cart__image-title" to={`/product/${item.id}`}>
-                                        <div className="cart__item-image-box">
-                                            <img className="cart__item-image" src={item.image} alt={item.name} />
-                                        </div>
-                                        <div className="cart__item-image-title-box">
-                                            <p className="cart__item-name">{item.name}</p>
-                                            <p className="cart__item-price">Price: ${item.price}</p>
-                                        </div>
-                                    </Link>
-                                    <div className="cart__details">
-                                        <p className="cart__item-total-price">Subtotal ${item.price * item.quantity}</p>
-                                        <div className="cart__details-input-box">
-                                            <PositiveNumberInput
-                                                value={item.quantity}
-                                                onChange={newQuantity => (handleChangeQuantity(item, newQuantity))}
-                                                onIncrement={() => handleIncrement(item)}
-                                                onDecrement={() => handleDecrement(item)}/>
-                                            <button className="btn cart__remove-item" onClick={() => removeFromCart(item.id)}>Remove</button>
-                                        </div>
+                    <ul className="cart__items">
+                        {cartItems.map(item => (
+                            <li className="cart__item" key={item.id}>
+                                <Link className="cart__image-title" to={`/product/${item.id}`}>
+                                    <div className="cart__item-image-box">
+                                        <img className="cart__item-image" src={item.image} alt={item.name} />
                                     </div>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
+                                    <div className="cart__item-image-title-box">
+                                        <p className="cart__item-name">{item.name}</p>
+                                        <p className="cart__item-price">Price: ${item.price}</p>
+                                    </div>
+                                </Link>
+                                <div className="cart__details">
+                                    <p className="cart__item-total-price">Subtotal ${item.price * item.quantity}</p>
+                                    <div className="cart__details-input-box">
+                                        <PositiveNumberInput
+                                            value={item.quantity}
+                                            onChange={newQuantity => (handleChangeQuantity(item, newQuantity))}
+                                            onIncrement={() => handleIncrement(item)}
+                                            onDecrement={() => handleDecrement(item)}/>
+                                        <button className="btn cart__remove-item" onClick={() => removeFromCart(item.id)}>Remove</button>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
                 <div className="cart__checkout-button-wrapper">
                     <Link className="btn cart__checkout-button" to='/shop'>Shop More</Link>
                     <Link className="btn cart__checkout-button" to='/checkout'>Go to Checkout</Link>
--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -1,12 +1,20 @@
 import { useCallback, useState } from 'react';
+import { useCart } from '../cart/cart-context';
+import { Link } from "react-router-dom";
 import './checkout.scss';
 
 export default function Checkout() {
     const [address, setAddress] = useState('');
     const [zip, setZip] = useState('');
+    const [shippingCost, setShippingCost] = useState(0);
     const [suggestions, setSuggestions] = useState([]);
     const [error, setError] = useState(null);
     const [showLoaderAddress, setShowLoaderAddress] = useState(false);
+
+    const {cart} = useCart();
+    const cartItems = cart.filter(item => item.quantity > 0);
+
+    const subtotal = cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
 
     const debounce = (func, delay) => {
         let timer;
@@ -27,7 +35,8 @@ export default function Checkout() {
         setShowLoaderAddress(true);
 
         try {
-            const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=${encodeURIComponent(query)}`);
+            const url = `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=${encodeURIComponent(query)}`;
+            const response = await fetch(url);
 
             if(!response.ok) {
                 throw new Error("Failed to fetch!");
@@ -50,10 +59,42 @@ export default function Checkout() {
         setSuggestions([]);
 
         if(place.address && place.address.postcode) {
-            setZip(place.address.postcode);
-        } else {
-            setZip('');
+            setZip(place.address.postcode || '');
+            calculateShipping(place.address.country);
         }
+    }
+
+    const fetchNeighboringCountries = async (selectedCountry) => {
+        console.log(`Fetching neighbors for: ${selectedCountry}`);
+        try {
+            const response = await fetch(`https://restcountries.com/v3.1/name/${selectedCountry}?fields=borders`);
+            if(!response.ok) throw new Error('Failed to fetch neighboring countries');
+
+            const data = await response.json();
+            console.log('Fetched data:', data);
+            return data[0]?.borders || [];
+        } catch (error) {
+            console.error('Error fetching neighboring countries: ', error);
+            return [];
+        }
+    }
+
+    const calculateShipping = async (selectedCountry) => {
+        if(!selectedCountry) return;
+
+        let cost = 25; //Default international shipping cost
+        if(selectedCountry === 'Serbia') {
+            cost = 5; //Same country shipping
+        } else {
+            const neighbors = await fetchNeighboringCountries(selectedCountry);
+            console.log("Neighboring countries:", neighbors);
+
+            if(neighbors.includes('SRB')) {
+                cost = 15; // Shipping costs 15$ if Serbian neighbor
+            }
+        }
+
+        setShippingCost(cost);
     }
 
     return (
@@ -61,56 +102,101 @@ export default function Checkout() {
             <div className="container">
                 <h1 className="page-title checkout__title">Checkout</h1>
                 <div className="checkout__wrapper">
-                    <form className="checkout__form">
-                        <div className="checkout__form-fields">
-                            <div className="input-wrapper">
-                                <input type="text" className="form-input" placeholder="Name" />
-                                <span className="form-message form-error form-input-error">Enter correct username.</span>
-                            </div>
-                            <div className="input-wrapper">
-                                <input type="email" className="form-input" placeholder="Email" />
-                                <span className="form-message form-error form-input-error">Enter correct email.</span>
-                            </div>
-                            <div className="input-wrapper">
-                                <input type="text" className="form-input" placeholder="Phone" />
-                                <span className="form-message form-error form-input-error">Enter old or new matching password.</span>
-                            </div>
-                            <div className="input-wrapper">
-                                <input
-                                    type="text"
-                                    className="form-input"
-                                    placeholder="Enter Address"
-                                    value={address}
-                                    onChange={e => {
-                                        setAddress(e.target.value);
-                                        debouncedFetch(e.target.value)}
-                                    } />
-                                {showLoaderAddress && <span className='loader my-account__loader'></span>}
-                                {error && <p className="error-message">{error}</p>}
-                                {!showLoaderAddress && suggestions.length > 0 && (
-                                    <ul className="input-wrapper__suggestions" id="suggestions">
-                                        {suggestions.map(place => (
-                                            <li
-                                                key={place.place_id}
-                                                className="input-wrapper__suggestions-item"
-                                                onClick={() => handleSelectAddress(place)}>{place.display_name}</li>
-                                        ))}
+                    {cartItems.length === 0 ? (
+                        <p>Your cart is empty! 
+                            <Link to='/shop'>Continue Shopping</Link>
+                        </p>) : (
+                        <div className="checkout__box">
+                            <form className="checkout__form">
+                                <div className="checkout__form-fields">
+                                    <div className="input-wrapper">
+                                        <input type="text" className="form-input" placeholder="First and Last name" />
+                                        <span className="form-message form-error form-input-error">Enter correct first and last name.</span>
+                                    </div>
+                                    <div className="input-wrapper">
+                                        <input type="email" className="form-input" placeholder="Email" />
+                                        <span className="form-message form-error form-input-error">Enter correct email.</span>
+                                    </div>
+                                    <div className="input-wrapper">
+                                        <input type="text" className="form-input" placeholder="Phone" />
+                                        <span className="form-message form-error form-input-error">Enter correct phone.</span>
+                                    </div>
+                                    <div className="input-wrapper">
+                                        <input
+                                            type="text"
+                                            className="form-input"
+                                            placeholder="Enter Address"
+                                            value={address}
+                                            onChange={e => {
+                                                setAddress(e.target.value);
+                                                debouncedFetch(e.target.value)}
+                                            } />
+                                        {showLoaderAddress && <span className='loader my-account__loader'></span>}
+                                        {error && <p className="error-message">{error}</p>}
+                                        {!showLoaderAddress && suggestions.length > 0 && (
+                                            <ul className="input-wrapper__suggestions" id="suggestions">
+                                                {suggestions.map(place => (
+                                                    <li
+                                                        key={place.place_id}
+                                                        className="input-wrapper__suggestions-item"
+                                                        onClick={() => handleSelectAddress(place)}>{place.display_name}</li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                        <span className="form-message form-error form-input-error">Enter correct address.</span>
+                                    </div>
+                                    <div className="input-wrapper">
+                                        <input
+                                            type="text"
+                                            className="form-input"
+                                            placeholder="ZIP"
+                                            value={zip}
+                                            onChange={(e) => setZip(e.target.value)}
+                                            />
+                                        <span className="form-message form-error form-input-error">Enter correct ZIP.</span>
+                                    </div>
+                                    <div className="input-wrapper">
+                                        <input type="text" className="form-input" placeholder="Company name" />
+                                        <span className="form-message form-error form-input-error">Enter correct company name.</span>
+                                    </div>
+                                </div>
+                            </form>
+                            <div className="checkout__info">
+                                <ul className="checkout__info-products">
+                                    {cartItems.map(item => (
+                                        <li className="checkout__info-product" key={item.id}>
+                                            <Link className='checkout__info-product-link' to={`/product/${item.id}`}>
+                                                <div className="checkout__info-product-image-box">
+                                                    <img className='checkout__info-product-image' src={item.image} alt={item.name} />
+                                                    <p className="checkout__info-product-title">{item.name}</p>
+                                                </div>
+                                                <div className="checkout__info-product-price-box">
+                                                    <span className="checkout__info-product-quantity">Quantity: ${item.quantity}</span>
+                                                    <span className="checkout__info-product-price">Price: ${item.price}</span>
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <div className="checkout__info-sub-total">
+                                    <ul className="checkout__info-sub-total-items">
+                                        <li className="checkout__info-sub-total-item">
+                                            <span className="checkout__info-sub-total-item-left">Subtotal</span>
+                                            <span className="checkout__info-sub-total-item-right">${subtotal}</span>
+                                        </li>
+                                        <li className="checkout__info-sub-total-item">
+                                            <span className="checkout__info-sub-total-item-left">Shipping</span>
+                                            <span className="checkout__info-sub-total-item-right">${shippingCost}</span>
+                                        </li>
                                     </ul>
-                                )}
-                                <span className="form-message form-error form-input-error">Enter old or new matching password.</span>
-                            </div>
-                            <div className="input-wrapper">
-                                <input
-                                    type="text"
-                                    className="form-input"
-                                    placeholder="ZIP"
-                                    value={zip}
-                                    onChange={(e) => setZip(e.target.value)}
-                                    />
-                                <span className="form-message form-error form-input-error">Enter old or new matching password.</span>
+                                </div>
+                                <div className="checkout__info-total">
+                                    <span className="checkout__info-total-left">Total</span>
+                                    <span className="checkout__info-total-right">${subtotal + shippingCost}</span>
+                                </div>
                             </div>
                         </div>
-                    </form>
+                    )}
                 </div>
             </div>
         </div>
